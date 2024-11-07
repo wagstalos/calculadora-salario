cf4f456334e4ec0cad5f042bc433f521983a6d1d
Wagner Paulo <wagstalos@hotmail.com>
2024-11-07 12:18:11 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function formatarSalario(element) {
+    let valor = element.value.replace(/\D/g, "");
+    valor = (parseFloat(valor) / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    element.value = valor;
+}
+
+function calcular() {
+    const idade = parseFloat(document.getElementById("idade").value);
+    const salarioText = document.getElementById("salario").value;
+    const salario = parseFloat(salarioText.replace(/[^\d]/g, ''));
+
+    if (!isNaN(idade) && !isNaN(salario)) {
+        const salarioReais = salario / 100;
+        const resultado = (idade * salarioReais) / 10;
+        const resultadoFormatado = resultado.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        document.getElementById("resultado").innerText = `Resultado: ${resultadoFormatado}`;
+    } else {
+        document.getElementById("resultado").innerText = "Por favor, insira valores válidos.";
+    }
+}
