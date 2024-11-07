b2155464b62653d2c4c16ed5d036311ca80a2226
Wagner Paulo <wagstalos@hotmail.com>
2024-11-07 19:57:05 -0300

    fix: changed english

--- js/main.js
+++ js/main.js
@@ -1,32 +1,42 @@
-window.addEventListener('DOMContentLoaded', ()=>{
-    handleIdateInput();
+window.addEventListener("DOMContentLoaded", () => {
+  handleIdateInput();
 });
 
-function handleIdateInput(){
-    const element = document.getElementById("idade");
-    element.addEventListener('keypress', (evt) => {
-        if(!/^\d+$/g.test(evt.key))
-            evt.preventDefault();
+function handleIdateInput() {
+  const element = document.getElementById("idade");
+  element.addEventListener("keypress", (evt) => {
+    if (!/^\d+$/g.test(evt.key)) evt.preventDefault();
+  });
+}
+
+function formatSalary(element) {
+  let value = element.value.replace(/\D/g, "");
+  value = (parseFloat(value) / 100).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
+  element.value = value;
+}
+
+function calculate() {
+  const age = parseFloat(document.getElementById("idade").value);
+  const salaryText = document.getElementById("salario").value;
+  const salary = parseFloat(salaryText.replace(/[^\d]/g, ""));
+
+  if (!isNaN(age) && !isNaN(salary)) {
+    const yearlySalary = (salary / 100) * 12;
+    const result = (age * yearlySalary) / 10;
+
+    const formattedResult = result.toLocaleString("pt-BR", {
+      style: "currency",
+      currency: "BRL",
     });
-}
 
-function formatarSalario(element) {
-    let valor = element.value.replace(/\D/g, "");
-    valor = (parseFloat(valor) / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    element.value = valor;
-}
-
-function calcular() {
-    const idade = parseFloat(document.getElementById("idade").value);
-    const salarioText = document.getElementById("salario").value;
-    const salario = parseFloat(salarioText.replace(/[^\d]/g, ''));
-
-    if (!isNaN(idade) && !isNaN(salario)) {
-        const salarioReais = (salario / 100)*12;
-        const resultado = (idade * salarioReais) / 10;
-        const resultadoFormatado = resultado.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-        document.getElementById("resultado").innerText = `Resultado: ${resultadoFormatado}`;
-    } else {
-        document.getElementById("resultado").innerText = "Por favor, insira valores válidos.";
-    }
+    document.getElementById(
+      "resultado"
+    ).innerText = `Resultado: ${formattedResult}`;
+  } else {
+    document.getElementById("resultado").innerText =
+      "Por favor, insira valores válidos.";
+  }
 }
